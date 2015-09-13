a62ef09428fbd6bcac13d3caa0bb23804ed6bb63
pratamawijaya <set.mnemonix@gmail.com>
2015-09-13 11:35:16 +0700

    commit just java

--- batch4/JustJava/app/src/main/java/com/pratamawijaya/justjava/MainActivity.java
+++ batch4/JustJava/app/src/main/java/com/pratamawijaya/justjava/MainActivity.java
@@ -6,13 +6,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+  private static final int HARGA_KOPI = 5;
   // 1. deklarasi button
-  Button btnOrder;
-  Button btnPlus;
+  private Button btnOrder;
+  private Button btnPlus, btnMinus;
+  private TextView txtQuantity, txtPrices;
+  private int quantityTotal = 0;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -21,9 +25,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // 2. inisiasi
     btnOrder = (Button) findViewById(R.id.btn_order);
     btnPlus = (Button) findViewById(R.id.btn_plus);
+    btnMinus = (Button) findViewById(R.id.btn_min);
+    txtQuantity = (TextView) findViewById(R.id.text_counter);
+    txtPrices = (TextView) findViewById(R.id.text_price);
 
     btnOrder.setOnClickListener(this);
     btnPlus.setOnClickListener(this);
+    btnMinus.setOnClickListener(this);
 
     //btnOrder.setOnClickListener(new View.OnClickListener() {
     //  @Override public void onClick(View view) {
@@ -42,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
    * method yang dijalankan ketika button order di klik
    */
   public void submitOrder() {
-    displayPrice(100);
+    displayPrice(quantityTotal * HARGA_KOPI);
   }
 
   /**
@@ -51,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
    * @param i angka
    */
   private void displayPrice(int i) {
-    Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_SHORT).show();
+    txtPrices.setText("Total : " + NumberFormat.getCurrencyInstance().format(i));
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
@@ -69,19 +77,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   }
 
   @Override public void onClick(View view) {
-    //if (view == btnOrder) {
-    //  submitOrder();
-    //} else if (view == btnPlus) {
-    //  Toast.makeText(MainActivity.this, "button plus", Toast.LENGTH_SHORT).show();
-    //}
-
-    switch (view.getId()) {
-      case R.id.btn_order:
-        submitOrder();
-        break;
-      case R.id.btn_plus:
-        Toast.makeText(MainActivity.this, "btn plus", Toast.LENGTH_SHORT).show();
-        break;
+    if (view == btnOrder) {
+      submitOrder();
+    } else if (view == btnPlus) {
+      incrementQuantity(1);
+    } else if (view == btnMinus) {
+      decreamentQuantity(1);
     }
+  }
+
+  /**
+   * mengurangi jumlah quantity
+   *
+   * @param i nilai
+   */
+  private void decreamentQuantity(int i) {
+    // decreament
+    if (quantityTotal > 0) {
+      quantityTotal = quantityTotal - i;
+      txtQuantity.setText("" + quantityTotal);
+    }
+  }
+
+  /**
+   * menambah jumlah quantity
+   *
+   * @param i nilai
+   */
+  private void incrementQuantity(int i) {
+    // increment
+    quantityTotal = quantityTotal + i;
+    txtQuantity.setText("" + quantityTotal);
   }
 }
--- batch4/MyApplication1/app/src/main/java/com/pratamawijaya/myapplication1/MainActivity.java
+++ batch4/MyApplication1/app/src/main/java/com/pratamawijaya/myapplication1/MainActivity.java
@@ -1,15 +1,44 @@
 package com.pratamawijaya.myapplication1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private LinearLayout container;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    container = (LinearLayout) findViewById(R.id.container);
+
+    TextView teks = new TextView(this);
+    teks.setText("Hello");
+    teks.setTextSize(22);
+    teks.setTextColor(Color.RED);
+
+    container.addView(teks);
+
+    // proses instance
+    //MonsterLaut monster1 = new MonsterLaut();
+    //monster1.setHp(100);
+    //monster1.setStrenght(250);
+    //monster1.setMp(100);
+    //
+    //MonsterLaut monster2 = new MonsterLaut(100, 150, 200);
+    //
+    //Gyarados gyarados1 = new Gyarados();
+    //gyarados1.setHp(1000);
+    //gyarados1.setMp(100);
+    //gyarados1.setStrenght(250);
+    //
+    //Toast.makeText(MainActivity.this, "" + gyarados1.attackSkillOne(), Toast.LENGTH_SHORT).show();
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
